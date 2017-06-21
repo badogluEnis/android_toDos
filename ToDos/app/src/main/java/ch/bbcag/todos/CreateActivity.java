cdac8ceec51c3848e29f27c76039be9d3c137c15
badogluEnis <enis.badoglu.gerber@gmail.com>
2017-06-21 16:35:44 +0200

    Datepicker integriert

--- ToDos/app/src/main/java/ch/bbcag/todos/CreateActivity.java
+++ ToDos/app/src/main/java/ch/bbcag/todos/CreateActivity.java
@@ -3,20 +3,40 @@ package ch.bbcag.todos;
 import android.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CreateActivity extends AppCompatActivity {
+
+    TextView date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
+        date = (EditText) findViewById(R.id.date);
+
+
+        date.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                showDatePickerDialog(v);
+                return false;
+            }
+        });
     }
+
 
     public void showDatePickerDialog(View v) {
         DatePickerFragment newFragment = new DatePickerFragment();
-        newFragment.show(getFragmentManager(), "datePicker");
+        newFragment.show(getFragmentManager(), "Datum wählen");
     }
+
 
     private void createTodo(View view){
 
