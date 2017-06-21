71f8f1879ff75aca8d5946a47ef06325d5deb712
badogluEnis <enis.badoglu.gerber@gmail.com>
2017-06-21 07:58:47 +0200

    Gui vom erstellen-Activity gemacht und an der Startacivity geabeitet

--- ToDos/app/src/main/java/ch/bbcag/todos/StartActvity.java
+++ ToDos/app/src/main/java/ch/bbcag/todos/StartActvity.java
@@ -1,5 +1,6 @@
 package ch.bbcag.todos;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -25,7 +26,8 @@ public class StartActvity extends AppCompatActivity {
     }
 
     private void addToDo(View view){
-
+        Intent intent = new Intent(this, CreateActivity.class);
+        startActivity(intent);
     }
 
 }
