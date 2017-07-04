571b9c5fbbedeb0adb090e6988c7b2e558d2ad6b
badogluEnis <enis.badoglu.gerber@gmail.com>
2017-07-04 13:50:52 +0200

    Angefangem mit Details Activity

--- ToDos/app/src/main/java/ch/bbcag/todos/DetailsActivity.java
+++ ToDos/app/src/main/java/ch/bbcag/todos/DetailsActivity.java
@@ -2,13 +2,55 @@ package ch.bbcag.todos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    EditText name;
+    EditText date;
+    EditText desc;
+
+    Button del;
+    Button edit;
+    Button done;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        name.setText("test");
+        date.setText("test");
+        desc.setText("test");
+
+        del.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                //todo löschen
+            }
+        });
+
+        edit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                //todo editieren
+            }
+        });
+
+        done.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                //todo als erledigt markieren
+            }
+        });
     }
 }
