21d21d3f8589da9fd2c352a227b20823bd2ab569
badogluEnis <enis.badoglu.gerber@gmail.com>
2017-07-04 13:52:53 +0200

    Detailsactivity kommentiert

--- ToDos/app/src/main/java/ch/bbcag/todos/DetailsActivity.java
+++ ToDos/app/src/main/java/ch/bbcag/todos/DetailsActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -16,15 +17,19 @@ public class DetailsActivity extends AppCompatActivity {
     Button edit;
     Button done;
 
+    Switch push;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        name.setText("test");
-        date.setText("test");
-        desc.setText("test");
+        name.setText("test"); //aus datenbank auslesen!!
+        date.setText("test"); //aus datenbank auslesen!!
+        desc.setText("test"); //aus datenbank auslesen!!
+
+        push.setChecked(false); //aus datenbank auslesen!!
 
         del.setOnClickListener(new View.OnClickListener() {
 
