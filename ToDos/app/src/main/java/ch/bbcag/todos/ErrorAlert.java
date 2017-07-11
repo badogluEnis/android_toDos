59f08a2cc7203cee9bf9093bbffef72d6e44adda
badogluEnis <enis.badoglu.gerber@gmail.com>
2017-07-11 08:58:35 +0200

    Eigene klasse für Erroralert gemacht um redundanzen zu vermeidem

---
+++ ToDos/app/src/main/java/ch/bbcag/todos/ErrorAlert.java
@@ -0,0 +1,27 @@
+package ch.bbcag.todos;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by bbadoe on 11.07.2017.
+ */
+
+public class ErrorAlert {
+
+    public static void showError(Context context){
+
+        //error anzeigen!!
+        AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle("Lade-Fehler");
+        alertDialog.setMessage("Ups, es ein Fehler im System aufgetreten, bitte starten Sie die App neu");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+    }
+}
